06b1a9709b14d96b27d24b4688d1762c252404e3
UnsEmp <1324847965@qq.com>
2021-12-03 09:17:51 +0800

    update 2021/12/3

---
+++ Task/Task.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+using namespace std;
+
+int main() {
+	ofstream out("Text.txt", ios::out);
+	for(int i = 1;i <= 99999;i++) {
+		string s = "str\\";
+		string ans = to_string(i);
+		for(int i = 0;i < 5 - ans.size();i++) s += '0';
+		s += ans;
+		s += ".png";
+		out << s << endl;
+	}
+	return 0;
+}
