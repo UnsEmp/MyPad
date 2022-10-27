2802ba67ba583ca245ef2be5205a3e4aaea306e1
UnsEmp <1324847965@qq.com>
2022-10-27 11:27:27 +0800

    update

--- codeforces/Balance.cpp
+++ codeforces/Balance.cpp
@@ -7,23 +7,30 @@
 
 #include<iostream>
 #include <set>
+#include <map>
 using namespace std;
 int q;
 
 int main() {
 	cin >> q;
 	set<long long> st;
+	map<long long, int> mp;
 	while(q--) {
 		char ch;
 		long long num;
 		cin >> ch >> num;
 		if(ch == '+') st.insert(num);
 		else {
-			long long sum = num;
-			while(st.find(sum) != st.end()) {
-				sum += num;
+			long long sum = 1;
+			if(mp.count(sum)) sum = mp[sum];
+			
+
+			
+			while(st.find(sum * num) != st.end()) {
+				sum ++;
 			}
-			cout << sum << "\n";
+			mp[sum * num] = sum;
+			cout << sum * num << "\n";
 		}
 	}
 }
